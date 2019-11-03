78a65b20852beb3f73275e768a392940e298fe98
LightCyberIndo <56459076+LightCyberIndo@users.noreply.github.com>
2019-11-04 02:31:58 +0700

    Add files via upload

---
+++ install/install.py
@@ -0,0 +1,45 @@
+import os,sys,time
+
+os.system('clear')
+
+print ""
+print ""
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('Selamat Datang user di program ELCei'.center(20))
+babi(' penginstallan bahan.....'.center(20))
+babi(' dimohon kepada user jangan meninggalkan'.center(20))
+babi(' terminal disaat penginstalan bahan'.center(20))
+print ""
+babi('Mohon tunggu beberapa saat sebelum'.center(20))
+babi(' penginstallan dimulai'.center(20))
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.8)
+
+babi('Tunggu sebentar....'.center(20))
+
+os.system('pkg update && pkg upgrade')
+os.system('pkg install git')
+os.system('pkg install lolcat')
+os.system('gem install lolcat')
+os.system('pkg install bash')
+os.system('pkg install python2')
+os.system('pkg install python')
+os.system('pkg install nano')
+os.system('pkg install neofetch')
+os.system('pip2 install lolcat')
+
+print ("Penginstalan selesai")
+print ("Silahkan kembali ke tool ELCei")
+print ("Dengan cara menulis sh ELCei.sh")
+
+exit()
