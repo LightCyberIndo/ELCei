d5e81388d69c6ce160b72153795c2f04b4b866ad
LightCyberIndo <56459076+LightCyberIndo@users.noreply.github.com>
2019-11-06 16:34:57 +0700

    Update run.py

--- tool/run.py
+++ tool/run.py
@@ -14,10 +14,12 @@ babi(' yang tahu username & password tool ini, mohon')
 babi(' untuk tidak menyebarluaskan, biarkan mereka yang')
 babi(' usaha mencari username & passwordnya sendiri')
 print
+print
 babi('Hargai author')
 babi(' karena memakai, tidak sesulit membuat >_<')
 print
-babi('Untuk versi kali ini, masih 11 tools, dan akan')
+print
+babi('Untuk versi kali ini ada 15 tools, dan akan')
 babi(' terus update, jadi nantikan update selanjutnya')
 babi('...........')
 
