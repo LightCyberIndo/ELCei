4879fec11a1ca5a6bca4a16eed3f63298e39d8b7
LightCyberIndo <56459076+LightCyberIndo@users.noreply.github.com>
2019-11-10 08:58:21 +0700

    Update run.py

--- tool/run.py
+++ tool/run.py
@@ -9,17 +9,21 @@ def babi(nob):
     time.sleep(0.1)
 
 babi('Selamat datang user di Light Cyber Indonesia Tools')
-babi(' Gunakan tools dengan bijak, dan mohon untuk kalian')
-babi(' yang tahu username & password tool ini, mohon')
-babi(' untuk tidak menyebarluaskan, biarkan mereka yang')
-babi(' usaha mencari username & passwordnya sendiri')
+print
+babi('Gunakan tools dengan bijak, dan mohon untuk kalian')
+print
+babi('yang tahu username & password tool ini, mohon')
+print
+babi('untuk tidak menyebarluaskan, biarkan mereka yang')
+print
+babi('usaha mencari username & passwordnya sendiri')
 print
 print
 babi('Hargai author')
 babi(' karena memakai, tidak sesulit membuat >_<')
 print
 print
-babi('Untuk versi kali ini ada 18 tools, dan akan')
+babi('Versi kali ini ada 21 tools, dan akan')
 babi(' terus update, jadi nantikan update selanjutnya')
 babi('...........')
 
