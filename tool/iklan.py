251f83e7f6bb8e71d4a8c76c97cef49dd7a00f69
LightCyberIndo <56459076+LightCyberIndo@users.noreply.github.com>
2020-01-14 16:48:35 +0700

    Add files via upload

---
+++ tool/iklan.py
@@ -0,0 +1,14 @@
+import os,sys,time
+
+os.system('clear')
+
+def babi(nob):
+  for e in nob:
+    sys.stdout.write(e)
+    sys.stdout.flush()
+    time.sleep(0.1)
+
+babi('IKLAN SEBENTAR...')
+print ""
+print ""
+babi('Apakah kalian ingin belajar membuat tool seperti saya?? Disini saya author membuka pembelajaran pembuatan tool installer seperti yang kalian gunakan saat ini, jika anda berminat silahkan DM Instagram saya @i.am.misetya atau whatsapp saya 0823-8623-4828 (Catatan : hanya membaca chat untuk yang mau belajar)')
